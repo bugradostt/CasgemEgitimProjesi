0b778e9f6c410f570b84b1e1215b0cdb912cda86
bugradostt <bugradostt@gmail.com>
2023-07-25 21:21:08 +0300

    templates edit 1

---
+++ CasgemEgitim.PresentationLayer/Controllers/VitrinCourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CasgemEgitim.PresentationLayer.Controllers
+{
+    public class VitrinCourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
