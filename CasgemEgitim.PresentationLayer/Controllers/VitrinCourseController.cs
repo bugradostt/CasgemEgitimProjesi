ebdf1eafa89abd89495062907feed1171444812c
bugradostt <bugradostt@gmail.com>
2023-07-25 22:03:42 +0300

    vitrin course list

--- CasgemEgitim.PresentationLayer/Controllers/VitrinCourseController.cs
+++ CasgemEgitim.PresentationLayer/Controllers/VitrinCourseController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CasgemEgitim.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CasgemEgitim.PresentationLayer.Controllers
 {
     public class VitrinCourseController : Controller
     {
+        readonly ICourseService _courseService;
+
+        public VitrinCourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var values = _courseService.TGetList().OrderByDescending(x => x.CourseId).ToList();
+            return View(values);
         }
     }
 }
